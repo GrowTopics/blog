f90965c2619e04e37e744221a31db7fbe7696b4e
CVXSL <70546159+CVXSL@users.noreply.github.com>
2021-11-11 15:25:38 -0700

    Update discord.tsx

--- src/pages/discord.tsx
+++ src/pages/discord.tsx
@@ -12,7 +12,7 @@ const DiscordPage = () => {
                     <h1><span>Grow</span>Topics Discord</h1>
                     <p>Click the button below to go to our official discord server!</p>
                     <div className="gtp-discord-card__actions">
-                        <OutboundLink href="https://discord.gg/X9fuz9HejU">
+                        <OutboundLink href="https://discord.gg/2NaDrfV6MK">
                             <Button type="filled" label="Discord Server" />
                         </OutboundLink>
                     </div>
